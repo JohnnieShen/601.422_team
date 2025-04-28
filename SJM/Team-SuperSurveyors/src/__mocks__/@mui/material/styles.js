bc7baf67e6ad0ff260264888c73f5fd408247ff9
Johnny <shenjiaming2003@gmail.com>
2025-04-28 13:53:26 -0400

    added stub for API KEY for mocking openAI

--- SJM/Team-SuperSurveyors/src/__mocks__/@mui/material/styles.js
+++
@@ -1,4 +0,0 @@
-export const createTheme = jest.fn(() => ({}));
-export const ThemeProvider = ({ children }) => <div>{children}</div>;
-export const styled = (Component) => Component;
-export const useTheme = () => ({});
--- SJM/Team-SuperSurveyors/src/__mocks__/firebase/auth.js
+++
@@ -1,8 +0,0 @@
-const m = {
-    getAuth: jest.fn(() => ({
-      currentUser: { uid: 'test-user' },   // what your components expect
-    })),
-  };
-  
-  module.exports = { __esModule: true, ...m, default: m };
-  
